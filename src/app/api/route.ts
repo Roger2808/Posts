d350a787edb1fa1d66ee6f61bb33bb7e51180346
Roger Monterroso <rogerclad228@gmail.com>
2025-08-18 22:15:21 -0600

    Validacion de datos

---
+++ src/app/api/route.ts
@@ -0,0 +1,26 @@
+import postgres from "postgres";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function POST(request: NextRequest) {
+    const data = await request.json();
+
+    if (!data.title || data.title.length === 0) {
+        return NextResponse.json({message: "Title can't be empty."});
+    }
+    if (data.title[0] !== data.title[0].toUpperCase()) {
+        return NextResponse.json({message: "El título debe empezar con mayúscula."});
+    }
+
+    if (!data.description || data.description.length === 0) {
+        return NextResponse.json({message: "La descripción no puede estar vacía."});
+    }
+
+    if (!data.author || data.author.length === 0) {
+        return NextResponse.json({message:"El autor no puede estar vacío."});
+    }
+    if (data.author[0] !== data.author[0].toUpperCase()) {
+        return NextResponse.json({message:"El autor debe empezar con mayúscula."});
+    }
+    
+}
