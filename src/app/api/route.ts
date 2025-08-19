9c9e413fe9c6b97db5dc05bfb13c8297c0386bed
Roger Monterroso <rogerclad228@gmail.com>
2025-08-19 09:05:55 -0600

    Abstraccion a nivel de funcion

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -5,31 +5,40 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
     const data = await request.json();
 
-    if (!data.title || data.title.length === 0) {
-        return NextResponse.json({message: "Title can't be empty."});
-    }
-    if (data.title[0] !== data.title[0].toUpperCase()) {
-        return NextResponse.json({message: "El título debe empezar con mayúscula."});
-    }
+    await validatePost(data);
 
-    if (!data.description || data.description.length === 0) {
-        return NextResponse.json({message: "La descripción no puede estar vacía."});
-    }
-
-    if (!data.author || data.author.length === 0) {
-        return NextResponse.json({message:"El autor no puede estar vacío."});
-    }
-    if (data.author[0] !== data.author[0].toUpperCase()) {
-        return NextResponse.json({message:"El autor debe empezar con mayúscula."});
-    }
-    
-    const connectionString = 'postgresql://postgres.ngqewaaalsclmldmhwbm:IbanezGio28*@aws-1-us-east-2.pooler.supabase.com:6543/postgres';
-    const sql = postgres(connectionString);
-
-    await sql`INSERT INTO Posts (title, description, author) VALUES (${data.title}, ${data.description}, ${data.author});`;
+    await savePost(data);
 
     return NextResponse.json({
         message: 'Post Save Correctly'
     });
 
+}
+
+async function savePost(data: any): Promise<void>{
+    const connectionString = 'postgresql://postgres.ngqewaaalsclmldmhwbm:IbanezGio28*@aws-1-us-east-2.pooler.supabase.com:6543/postgres';
+    const sql = postgres(connectionString);
+
+    await sql`INSERT INTO Posts (title, description, author) VALUES (${data.title}, ${data.description}, ${data.author});`;
+
+}
+
+async function validatePost(data: any){
+    if (!data.title || data.title.length === 0) {
+        throw new Error("Title can't be empty.");
+    }
+    if (data.title[0] !== data.title[0].toUpperCase()) {
+        throw new Error("Titulo debe iniciar con mayuscula.");
+    }
+
+    if (!data.description || data.description.length === 0) {
+        throw new Error("Description can't be empty.");
+    }
+
+    if (!data.author || data.author.length === 0) {
+        throw new Error("Author can't be empty.");
+    }
+    if (data.author[0] !== data.author[0].toUpperCase()) {
+        throw new Error("Autor debe iniciar con mayuscula.");
+    }
 }
